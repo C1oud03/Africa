0b0a4cb07f71a3d5b77ee23efe9ff3a6512be7e7
C1oud03 <c1oud03@yandex.ru>
2021-05-07 17:32:43 +0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Семён Рытенков on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
