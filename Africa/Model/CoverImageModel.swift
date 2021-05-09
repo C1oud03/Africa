24450425157d35b2e74455497a1cc7a2274d11c6
C1oud03 <c1oud03@yandex.ru>
2021-05-09 15:56:08 +0400

    added bundle decoder extension, cover image model, cover image view

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Семён Рытенков on 09.05.2021.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
