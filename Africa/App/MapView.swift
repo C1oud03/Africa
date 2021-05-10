a298c55a33c837fd118ebc66e6ef31fb69e8fc9b
C1oud03 <c1oud03@yandex.ru>
2021-05-10 18:04:55 +0400

    added national park location model and map to map view

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,15 +6,43 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     
     // MARK: - PROPERTIES
     
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.00286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     // MARK: - BODY
     
     var body: some View {
-        Text("Map")
+            
+        Map(
+            coordinateRegion: $region,
+            annotationItems: locations,
+            annotationContent: { item in
+                MapAnnotation(
+                    coordinate: item.location,
+                    content: {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 32, height: 32)
+                    } //: CONTENT
+                )
+            } //: ANNOTATION CONTENT
+        )
+        .edgesIgnoringSafeArea(.all)
+        
     } //: BODY
 }
 
