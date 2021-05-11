eeaa23d330a2de280e82154112bf8a9db191701e
C1oud03 <c1oud03@yandex.ru>
2021-05-11 12:25:38 +0400

    added animal grid item view and complex grid layout system with toolbar buttons

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,37 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Семён Рытенков on 11.05.2021.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    } //: BODY
+}
+
+// MARK: - PREVIEW
+
+struct AnimalGridItemView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
